cb3c9be7d9b93b72caff4a6155ebc0de5b60aa0f
hassan-khurram-hsol <hassan.khurram@heuristicsol.com>
2024-03-17 03:38:42 +0000

    Sun Mar 17 03:38:42 AM UTC 2024

--- scripts/create-sudo-user.sh
+++ scripts/create-sudo-user.sh
@@ -13,15 +13,6 @@ sudo adduser "$username_input"
 # Add the user to the sudo group
 sudo usermod -aG sudo "$username_input"
 
-# Add the user to sudoers file if requested
-read -p "STEP 4: Do you want to add current user '${username_input}' to visudo? (y/n):" answer
-if [ "$answer" = "y" ]; then
-    echo "Adding $username_input to sudoers file..."
-    echo "$username_input ALL=(ALL:ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers > /dev/null
-    echo "User $username_input added to sudoers file."
-fi
-
-# Switch to the newly created user and execute the command
 sudo -u "$username_input" sh -c "$(curl -fsSL https://raw.githubusercontent.com/hassankhurram/devops/main/scripts/ubuntu-minimal.sh)"
 
 sudo reboot now

ff069bf47b16acef8cac6a22b6a654c777ccd111
hassan-khurram-hsol <hassan.khurram@heuristicsol.com>
2024-03-17 03:30:19 +0000

    Sun Mar 17 03:30:19 AM UTC 2024

--- scripts/create-sudo-user.sh
+++ scripts/create-sudo-user.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Check if script is run as root
-if [ "$(id -u)" -ne 0 ]; then
-    echo "This script must be run as root." >&2
-    exit 1
-fi
-
 # Main script
 read -p "Enter the username to create: " username_input
 if [ -z "$username_input" ]; then
@@ -19,7 +13,16 @@ sudo adduser "$username_input"
 # Add the user to the sudo group
 sudo usermod -aG sudo "$username_input"
 
+read -p "STEP 4: Do you want to add current user '${username_input}' to visudo? (y/n):" answer
+if [ "$answer" = "y" ]; then
+        echo "Adding $username_input to sudoers file..."
+        echo "$username_input ALL=(ALL:ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers > /dev/null
+        echo "User $username_input added to sudoers file."
+fi
 
 echo "User '$username_input' created and added to the sudo group successfully."
+
+
+
 
 su - $username_input

a8f2edd32f2d9496650e106b15b0306718d83d19
hassankhurram <hassankhurram@ymail.com>
2024-08-23 07:22:52 +0000

    Fri Aug 23 07:22:52 AM UTC 2024 Hassan Khurram

--- scripts/selenium-fix/chrome-and-driver-fix.sh
+++ scripts/selenium-fix/chrome-and-driver-fix.sh
@@ -1,41 +1,43 @@
 #!/bin/bash
 
-# Author github.com/hassankhurram
-# Created @ 2023-11-13 10:22:14
-# Description: This script installs the latest version of Chrome on WSL2.
-# It downloads the browser and driver from Google's official website.
-# It then moves the browser and driver to the appropriate locations.
-# It also sets the permissions for the browser and driver.
-# This script is for educational purposes only.
-# Please use it responsibly.
+# Author: github.com/hassankhurram
+# Created: 2023-11-13 10:22:14
+# Description: This script installs the specified version of Chrome on WSL2.
+# It downloads the browser and driver from Google's official website,
+# moves them to the appropriate locations, and sets the necessary permissions.
+# This script is for educational purposes only. Use it responsibly.
 
 # Set the Chrome version to download.
+CHROMIUM_VERSION="128.0.6613.84" # Update the version here.
 
-CHROMIUM_VERSION="128.0.6613.84" # update the version here.
-
+# Function to clean up Chrome browser files
 function cleanupChromeBrowser {
     rm -f chrome-linux64.zip
-    rm -rf chrome-browser 
+    rm -rf chrome-linux64
 }
 
+# Function to clean up ChromeDriver files
 function cleanupChromeDriver {
     rm -f chromedriver-linux64.zip
-    rm -rf chromedriver-linux64 
+    rm -rf chromedriver-linux64
 }
-echo "initialization"node 
+
+# Initialize and update package list
+echo "Initializing..."
 sudo apt update
 sudo apt install -y wget gnupg
 
-echo "Downloading Chrome Browser"
+# Download and install Chrome browser
+echo "Downloading Chrome Browser..."
 cleanupChromeBrowser
 wget https://storage.googleapis.com/chrome-for-testing-public/$CHROMIUM_VERSION/linux64/chrome-linux64.zip
 unzip chrome-linux64.zip
-mv chrome-linux64 chrome-browser
-sudo mv chrome-browser/chrome /usr/bin/chrome
+sudo mv chrome-linux64/chrome /usr/bin/chrome
 sudo chmod +x /usr/bin/chrome
 cleanupChromeBrowser
 
-echo "Downloading Chrome Driver"
+# Download and install ChromeDriver
+echo "Downloading ChromeDriver..."
 cleanupChromeDriver
 wget https://storage.googleapis.com/chrome-for-testing-public/$CHROMIUM_VERSION/linux64/chromedriver-linux64.zip
 unzip chromedriver-linux64.zip
@@ -43,4 +45,4 @@ sudo mv chromedriver-linux64/chromedriver /usr/local/bin/chromedriver
 sudo chmod +x /usr/local/bin/chromedriver
 cleanupChromeDriver
 
-echo "Installed Chrome on WSL2"
+echo "Chrome and ChromeDriver installed on WSL2."
